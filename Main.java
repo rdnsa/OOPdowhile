59bd42a6e8de5028329eaa9879124ac1fad70f84
rdnsa <165114761+rdnsa@users.noreply.github.com>
2024-12-17 08:51:12 +0700

    Do While
    Mengimplementasikan Object Oriented Programming menggunakan do while

---
+++ Main.java
@@ -0,0 +1,9 @@
+class DoWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("i ke-" + i);
+            i++;
+        } while (i <= 5);
+    }
+}
